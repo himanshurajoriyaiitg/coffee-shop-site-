9ab9c5778b39f399c32d75ddce4476e8d07eb938
himanshurajoriyaiitg <himanshuiitg2006@gmail.com>
2025-01-18 13:32:35 +0530

    fixing the nav bar

--- script.js
+++ script.js
@@ -7,11 +7,49 @@ function hidecontent(){
     sidebar.style.display='none'
 }
 
-const swiper = new Swiper('.swiper', {
+
+let item1=document.getElementById('sidebaritem1');
+  item1.addEventListener('click',()=>{
+       hidecontent();
+       item1.click();
+
+  })
+  let item2=document.getElementById('sidebaritem2');
+  item2.addEventListener('click',()=>{
+       hidecontent();
+       item2.click();
+
+  })
+  let item3=document.getElementById('sidebaritem3');
+  item3.addEventListener('click',()=>{
+       hidecontent();
+       item3.click();
+
+  })
+  let item4=document.getElementById('sidebaritem4');
+  item4.addEventListener('click',()=>{
+       hidecontent();
+       item4.click();
+
+  })
+  let item5=document.getElementById('sidebaritem5');
+  item5.addEventListener('click',()=>{
+       hidecontent();
+       item5.click();
+
+  })
+  let item6=document.getElementById('sidebaritem6');
+  item6.addEventListener('click',()=>{
+       hidecontent();
+       item6.click();
+
+  })
+
+
+  const swiper = new Swiper('.swiper', {
     // Optional parameters
     direction: 'horizontal',
     loop: true,
-    spaceBetween:25,
   
     // If we need pagination
     pagination: {
@@ -23,6 +61,10 @@ const swiper = new Swiper('.swiper', {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
+  
+   
+   
+ 
     breakpoints:{
         0:{
             slidesPerView:1
